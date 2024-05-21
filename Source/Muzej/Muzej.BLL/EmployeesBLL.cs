3ab73f49429346ae964cdcc5afab9ea2b524a9e8
NovakPetar <petar.novak@fer.hr>
2024-05-21 15:55:16 +0200

    Added BLL library

---
+++ Source/Muzej/Muzej.BLL/EmployeesBLL.cs
@@ -0,0 +1,25 @@
+﻿using Muzej.DomainObjects;
+using Muzej.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muzej.BLL
+{
+    public class EmployeesBLL
+    {
+        private IEmployeesRepository _employeesRepository;
+        public EmployeesBLL(IRepositoryWrapper repositoryWrapper)
+        {
+            _employeesRepository = repositoryWrapper.Employees;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeesRepository.GetEmployee(id);
+        }
+    }
+}
